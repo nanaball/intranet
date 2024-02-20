7f02a7326c8269c97f9d88d4667dca39f7243f02
qwerdf1133 <anwltjdwhdgus@gmail.com>
2024-02-20 17:23:28 +0900

    sql문 테이블 명령문 추가 VO 수정

--- src/main/java/com/bitc/intranet/vo/MemberVO.java
+++ src/main/java/com/bitc/intranet/vo/MemberVO.java
@@ -4,14 +4,14 @@ import java.util.Date;
 
 public class MemberVO {
 
-	private int uNo;			// 사원번호
-	private String uId;			// 아이디
-	private String uPw;			// 비밀번호
-	private String uEmail;		// 이메일
-	private String uPhone;		// 전화번호
-	private String uName;		// 이름
-	private String uDep;		// 부서
-	private String uJob;		// 직책
-	private Date uDate;			// 입사일
+	private int uno;			// 사원번호
+	private String uid;			// 아이디
+	private String upw;			// 비밀번호
+	private String uemail;		// 이메일
+	private String uphone;		// 전화번호
+	private String uname;		// 이름
+	private String udep;		// 부서
+	private String ujob;		// 직책
+	private Date udate;			// 입사일
 	
 }
