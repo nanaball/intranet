76bebcddc32d9879b760ff080613eb5f966c4b9b
jeongaeran <baby1750232@gmail.com>
2024-02-21 11:40:46 +0900

    model DB

--- intranet.sql
+++ intranet.sql
@@ -1,3 +1,13 @@
 create database intranet;
 
+CREATE TABLE model(
+	mNo		INT PRIMATY KEY	auto_increment,--게시글번호
+	mName	VARCHAR(150) 	NOT NULL,
+	price	INT				NOT NULL,
+	stock	INT				DEFAULT 0,
+	soldout char(1) DEFAULT	'Y',		--솔드아웃 여부 -> Y 구매불가능 비활성화
+	pay     char(1) DEFAULT	'Y',        --페이된것-> Y
+	pDate	TIMESTAMP default now()
+);
+
 
