3870e74cb2ee88a450c85addf84877c4582f8b4b
nanaball <nanaball127@gamil.com>
2024-02-21 09:03:31 +0900

    OFFICIAL DB MODIFY

--- intranet.sql
+++ intranet.sql
@@ -3,14 +3,14 @@ create database intranet;
 
 -- 공문 db
 CREATE TABLE IF NOT EXISTS official(
-	ono		PRIMARY KEY INT AUTO_INCREMENT,			-- 공문번호
-	dep	VARCHAR(20) NOT NULL,		-- 부서
-	job	VARCHAR(20) NOT NULL,		-- 직책
-	title	VARCHAR(50) NOT NULL, 		-- 공문제목
-	content TEXT NOT NULL, 				-- 공문내용
-	writer  VARCHAR(20) NOT NULL, 		-- 작성자
-	approve	VARCHAR(20) NOT NULL,		-- 결재/승인 여부 
-	date datestamp not null,			-- 작성시간
+	ono	INT	PRIMARY KEY  AUTO_INCREMENT,			-- 공문번호
+	dep	VARCHAR(20) NOT NULL,						-- 부서
+	job	VARCHAR(20) NOT NULL,						-- 직책
+	title	VARCHAR(50) NOT NULL, 					-- 공문제목
+	content TEXT NOT NULL, 							-- 공문내용
+	writer  VARCHAR(20) NOT NULL, 					-- 작성자
+	approve	VARCHAR(20) NOT NULL,					-- 결재/승인 여부 
+	date datestamp not null DEFAULT now(),			-- 작성시간
 	FOREIGN KEY (id) REFERENCES members(id)
 );
 
