b49460ed7fdfb2bf2a03180ff06eb06441b2fb9a
nanaball <nanaball127@gamil.com>
2024-02-22 14:18:00 +0900

    _MemberMapper test 완

--- src/main/java/com/bitc/intranet/service/MemberService.java
+++ src/main/java/com/bitc/intranet/service/MemberService.java
@@ -1,5 +1,33 @@
 package com.bitc.intranet.service;
 
-public class MemberService {
+import java.util.List;
 
+import com.bitc.intranet.vo.MemberVO;
+
+public interface MemberService {
+	
+
+
+	/**
+	 * 
+	 * @param vo - 회원 가입한 회원 정보
+	 * uid, upw, uname, uemail, uphone, udep, ujob, udate
+	 */
+	void addMember(MemberVO vo) throws Exception;
+
+	/**
+	 * 
+	 * @return - 등록된 전체 회원 목록
+	 * @throws Exception
+	 */
+	List<MemberVO> list() throws Exception;
+	
+	/**
+	 * 
+	 * @param uid - 회원 아이디
+	 * @param mno - 회원번호 
+	 * @return
+	 * @throws Exception
+	 */
+	MemberVO readMember (String uid, int uno) throws Exception;
 }
