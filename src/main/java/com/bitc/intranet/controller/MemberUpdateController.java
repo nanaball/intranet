55a49319f92c3cc9da22a318b09df395910fd0cd
qwerdf1133 <anwltjdwhdgus@gmail.com>
2024-03-05 13:55:44 +0900

    commit

--- src/main/java/com/bitc/intranet/controller/MemberUpdateController.java
+++ src/main/java/com/bitc/intranet/controller/MemberUpdateController.java
@@ -1,26 +1,19 @@
 package com.bitc.intranet.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.bitc.intranet.service.MemberUpdateService;
-import com.bitc.intranet.vo.MemberVO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/member")
-@RequiredArgsConstructor
 @Slf4j
 public class MemberUpdateController {
 
-	private final MemberUpdateService mus;
-	
 	@GetMapping("memberUpdate")
 	public void memberUpdate() {}
 	
