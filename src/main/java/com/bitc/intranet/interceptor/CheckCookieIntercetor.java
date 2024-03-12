39fc0da7e534621776cbc84aeb88a5de4b9110f2
jinseong-1 <dwlstjdb9743@gmail.com>
2024-03-12 16:05:57 +0900

    interceptor fail

---
+++ src/main/java/com/bitc/intranet/interceptor/CheckCookieIntercetor.java
@@ -0,0 +1,44 @@
+package com.bitc.intranet.interceptor;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.WebUtils;
+
+import com.bitc.intranet.mapper.MemberMapper;
+import com.bitc.intranet.vo.MemberVO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CheckCookieIntercetor implements HandlerInterceptor {
+	
+	// 로그인 확인
+	@Autowired
+	MemberMapper mapper;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("")!=null) {
+			log.info("이미 로그인 된 사용자");
+			return true;
+		}
+		
+		Cookie cookie = WebUtils.getCookie(request, "signInCooie");
+		if(cookie != null) {
+			String uid = cookie.getValue();
+			MemberVO userInfo = mapper.readMember(uid);
+			session.setAttribute("userInfo", userInfo);
+			log.info("Cookie로 session 정보 등록 : {}",userInfo);
+		}
+		return true;
+	}
+
+	
+}
---
+++ src/main/java/com/bitc/intranet/interceptor/SignIninterceptor.java
@@ -0,0 +1,59 @@
+package com.bitc.intranet.interceptor;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bitc.intranet.vo.LoginDTO;
+import com.bitc.intranet.vo.MemberVO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SignIninterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("userInfo") != null) {
+			session.removeAttribute("userInfo");
+		}
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		HttpSession session = request.getSession();
+		MemberVO userInfo = (MemberVO)session.getAttribute("userInfo");
+		log.info("------ interceptor user info : {}",userInfo);
+		
+		ModelMap modelObj = modelAndView.getModelMap();
+		LoginDTO dto = (LoginDTO)modelObj.get("loginDTO");
+		log.info("------ interceptor login dto : {}",dto);
+		
+		if(userInfo != null) {
+			// 정상적으로 로그인 된 상태
+			if(dto.isUserCookie()) {
+				// 자동로그인 요청
+				Cookie cookie = new Cookie("signInCookie",userInfo.getUid());
+				cookie.setPath("/");
+				cookie.setMaxAge(60*60*8);
+				response.addCookie(cookie);
+			}
+			
+		}else {
+			// 잘못된 사용자 정보로 로그인 요청
+			String message = "로그인 실패";
+			modelAndView.addObject("message",message);
+			modelAndView.setViewName("/user/signIn");
+		}
+	}
+	
+}
