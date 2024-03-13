02c817fef08ac4d71b9bcee00f6f92702ee24d07
jinseong-1 <dwlstjdb9743@gmail.com>
2024-03-13 11:59:38 +0900

    각종 게시판 접근에 interceptor 적용

--- src/main/java/com/bitc/intranet/interceptor/AuthInterceptor.java
+++ src/main/java/com/bitc/intranet/interceptor/AuthInterceptor.java
@@ -27,54 +27,37 @@ public class AuthInterceptor implements HandlerInterceptor {
 		// 프로젝트 경로
 		String contextPath = request.getContextPath();
 		// session에 저장된 로그인된 사용자 정보
-		Object obj = request.getSession().getAttribute("userInfo");
+		Object obj = request.getSession().getAttribute("loginMember");
 		if(obj == null) {
 			// 미로그인 사용자
-			response.sendRedirect(contextPath+"/member/login");
+			response.sendRedirect(contextPath+"/");
 			return false;
 		}else {
 			// 로그인 된 사용자
-			if(requestURI.equals(contextPath+"/Notice/noticeRegist")) {
+			if(requestURI.startsWith(contextPath+"/Notice/")) {
+				// 새 원본글 작성 요청
+				return true;
+			}
+			if(requestURI.startsWith(contextPath+"/free/")) {
+				// 새 원본글 작성 요청
+				return true;
+			}
+			if(requestURI.startsWith(contextPath+"/accuse/")) {
+				// 새 원본글 작성 요청
+				return true;
+			}
+			if(requestURI.startsWith(contextPath+"/Welfare/")) {
+				// 새 원본글 작성 요청
+				return true;
+			}
+			if(requestURI.startsWith(contextPath+"/message/")) {
 				// 새 원본글 작성 요청
 				return true;
 			}
 			
-			// 답변글 요청, 수정 요청, 삭제 요청
-			String num = request.getParameter("bno");
-			/*
-			 * [-+]? 하나 또는 없거나
-			 * 문자열 맨 앞에 + 문자와 - 문자를 허용한다.
-			 * \\d*
-			 * 문자열에 최소한 하나 이상의 숫자가 존재해야한다.
-			 * \\. \\d+
-			 * - 0 ~ 1개의 소수점을 허용한다.
-			 */
-			String regex = "[-+]?\\d*\\.?\\d+";
-			if(num != null && !num.trim().equals("") && num.matches(regex)) {
-				int bno = Integer.parseInt(num);
-				
-				if(requestURI.equals(contextPath+"/board/replyRegister")) {
-					log.info("답변글 작성 요청");
-					return true;
-				}
-				
-				// 수정, 삭제 요청
-				NoticeVO notice = mapper.read(bno);
-				// 로그인된 사용자 정보
-				MemberVO userInfo = (MemberVO)obj;
-				if(notice.getBno() == userInfo.getUno()) {
-					// 수정 삭제 권한이 있는 사용자 - 로그인 된 사용자가 게시글을 작성한 사용자
-					return true;
-				}else {
-					response.sendRedirect(contextPath+"/board/read?bno="+bno);
-					return false;
-				}
-				
-			}else {
-				response.sendRedirect(contextPath+"/board/listReply");
-				return false;
-			}
-		}
+		}// else end
+		log.info("AuthInterceptor preHandler END");
+		return true;
 		
 		// 모든 경우의 수에 return 값이 존재하기에 필요없어 빨간줄이 뜨는 것
 		// log.info("AuthInterceptor preHandler END");
